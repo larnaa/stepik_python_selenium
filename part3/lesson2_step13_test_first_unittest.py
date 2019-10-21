0e9f2630bfe1678ee315a8bbe7ef0d4ceaf7d4a5
larnaa <larnaa@localhost.localdomain>
2019-10-21 15:55:46 +0300

    Added fill_form method, changed locator search

--- part3/lesson2_step13_test_first_unittest.py
+++ part3/lesson2_step13_test_first_unittest.py
@@ -3,9 +3,7 @@
 import unittest
 from selenium import webdriver
 import time
-
-link = 'http://suninjuly.github.io/registration1.html'
-link_bug_page = 'http://suninjuly.github.io/registration2.html'
+from selenium.webdriver.common.by import By
 
 
 class TestUniqueSelectors(unittest.TestCase):
@@ -13,41 +11,32 @@ class TestUniqueSelectors(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def test_registration(self):
+    def fill_form(self, link):
         browser = self.driver
         browser.get(link)
 
-        first_name = browser.find_element_by_css_selector('.first_block .first')
-        first_name.send_keys('Kesa')
-        last_name = browser.find_element_by_css_selector('.first_block .second')
-        last_name.send_keys('Lisa')
-        email = browser.find_element_by_css_selector('.third_class .third')
-        email.send_keys('KL@google.com')
+        browser.find_element(By.CSS_SELECTOR, '.first_block .first').send_keys('Kesa')
+        browser.find_element(By.CSS_SELECTOR, '.first_block .second').send_keys('Lisa')
+        browser.find_element(By.CSS_SELECTOR, '.third_class .third').send_keys('KL@google.com')
 
         button = browser.find_element_by_css_selector("button.btn")
         button.click()
         time.sleep(2)
 
         welcome_text = browser.find_element_by_tag_name('h1')
-        self.assertEqual("Congratulations! You have successfully registered!", welcome_text.text)
+        return welcome_text.text
+
+    def test_registration(self):
+        link = 'http://suninjuly.github.io/registration1.html'
+        registration_result = self.fill_form(link)
+
+        self.assertEqual("Congratulations! You have successfully registered!", registration_result)
 
     def test_registration_bug(self):
-        browser = self.driver
-        browser.get(link_bug_page)
+        link = 'http://suninjuly.github.io/registration2.html'
+        registration_result = self.fill_form(link)
 
-        first_name = browser.find_element_by_css_selector('.first_block .first')
-        first_name.send_keys('Kesa')
-        last_name = browser.find_element_by_css_selector('.first_block .second')
-        last_name.send_keys('Lisa')
-        email = browser.find_element_by_css_selector('.third_class .third')
-        email.send_keys('KL@google.com')
-
-        button = browser.find_element_by_css_selector("button.btn")
-        button.click()
-        time.sleep(2)
-
-        welcome_text = browser.find_element_by_tag_name('h1')
-        self.assertEqual("Congratulations! You have successfully registered!", welcome_text.text)
+        self.assertEqual("Congratulations! You have successfully registered!", registration_result)
 
     def tearDown(self):
         self.driver.close()
