6b905b90b337484be9a1b4523543773ab2cdf463
larnaa <larnaa@localhost.localdomain>
2019-10-21 16:17:40 +0300

    Added explanation to code and error message

--- part3/lesson2_step13_test_first_unittest.py
+++ part3/lesson2_step13_test_first_unittest.py
@@ -12,14 +12,14 @@ class TestUniqueSelectors(unittest.TestCase):
 
     def fill_form(self, link):
         browser = self.driver
-        browser.implicitly_wait(5)
+        browser.implicitly_wait(5)  # waiting for welcome_text a load
         browser.get(link)
 
-        browser.find_element(By.CSS_SELECTOR, '.first_block .first').send_keys('Kesa')
-        browser.find_element(By.CSS_SELECTOR, '.first_block .second').send_keys('Lisa')
-        browser.find_element(By.CSS_SELECTOR, '.third_class .third').send_keys('KL@google.com')
+        browser.find_element(By.CSS_SELECTOR, '.first_block .first').send_keys('Kesa')  # field first name
+        browser.find_element(By.CSS_SELECTOR, '.first_block .second').send_keys('Lisa')  # field last name
+        browser.find_element(By.CSS_SELECTOR, '.third_class .third').send_keys('KL@google.com')  # field email
 
-        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()  # click button submit
 
         welcome_text = browser.find_element(By.TAG_NAME, 'h1').text
         return welcome_text
@@ -28,13 +28,17 @@ class TestUniqueSelectors(unittest.TestCase):
         link = 'http://suninjuly.github.io/registration1.html'
         registration_result = self.fill_form(link)
 
-        self.assertEqual("Congratulations! You have successfully registered!", registration_result)
+        self.assertEqual(
+            'Congratulations! You have successfully registered!', registration_result,  # reg results
+            'Registration is failed')  # message error
 
     def test_registration_bug(self):
         link = 'http://suninjuly.github.io/registration2.html'
         registration_result = self.fill_form(link)
 
-        self.assertEqual("Congratulations! You have successfully registered!", registration_result)
+        self.assertEqual(
+            'Congratulations! You have successfully registered!', registration_result,  # reg results
+            'Registration is failed')  # message error
 
     def tearDown(self):
         self.driver.close()
