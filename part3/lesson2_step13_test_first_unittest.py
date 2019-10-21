be9f81ef6f93b8db86eac579d3c8e453259d7ef0
larnaa <larnaa@localhost.localdomain>
2019-10-21 16:02:28 +0300

    Added implicitly_wait, removed time.sleep

--- part3/lesson2_step13_test_first_unittest.py
+++ part3/lesson2_step13_test_first_unittest.py
@@ -2,7 +2,6 @@
 
 import unittest
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
 
 
@@ -13,18 +12,17 @@ class TestUniqueSelectors(unittest.TestCase):
 
     def fill_form(self, link):
         browser = self.driver
+        browser.implicitly_wait(5)
         browser.get(link)
 
         browser.find_element(By.CSS_SELECTOR, '.first_block .first').send_keys('Kesa')
         browser.find_element(By.CSS_SELECTOR, '.first_block .second').send_keys('Lisa')
         browser.find_element(By.CSS_SELECTOR, '.third_class .third').send_keys('KL@google.com')
 
-        button = browser.find_element_by_css_selector("button.btn")
-        button.click()
-        time.sleep(2)
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
 
-        welcome_text = browser.find_element_by_tag_name('h1')
-        return welcome_text.text
+        welcome_text = browser.find_element(By.TAG_NAME, 'h1').text
+        return welcome_text
 
     def test_registration(self):
         link = 'http://suninjuly.github.io/registration1.html'
