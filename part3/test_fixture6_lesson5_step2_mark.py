17ce4d882d17107ec2fd399e827be56142beec19
larnaa <larnaa@localhost.localdomain>
2019-10-22 14:01:53 +0300

    Added mark win10

--- part3/test_fixture6_lesson5_step2_mark.py
+++ part3/test_fixture6_lesson5_step2_mark.py
@@ -23,6 +23,7 @@ class TestMainPage1():
         browser.find_element_by_css_selector("#login_link")
 
     @pytest.mark.regression
+    @pytest.mark.win10
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
