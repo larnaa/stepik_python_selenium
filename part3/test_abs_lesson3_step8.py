77c4cb968e88faf885e38701df46f2af9f3d1602
larnaa <larnaa@localhost.localdomain>
2019-10-22 11:22:34 +0300

    Short version lesson2_test_suits_test_abs_project.py

---
+++ part3/test_abs_lesson3_step8.py
@@ -0,0 +1,9 @@
+# https://stepik.org/lesson/193188/step/8?unit=167629
+
+
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
